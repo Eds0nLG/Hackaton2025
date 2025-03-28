c784042d4e6108831767daca56fc62aef3269482
Anonim <anonim>
2025-03-28 15:48:16 -0600

    Initial Commit

---
+++ Mediapp/MediappApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediappApp.swift
+//  Mediapp
+//
+//  Created by Alumno on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MediappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
